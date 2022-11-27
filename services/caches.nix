efce6a40910bd00952f9e2f1ba280fd84f7d21d2
Sascha <GlancingMind@outlook.com>
2022-11-27 14:21:29 +0100

    Add nix-community and own cache substituters

--- services/caches.nix
+++ services/caches.nix
@@ -1,14 +1,15 @@
 { pkgs, ...}:
 {
   nix.settings = {
-    trusted-substituters = [
-      "https://ros.cachix.org"
-    ];
     substituters = [
       "https://ros.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://glancingmind.cachix.org"
     ];
     trusted-public-keys = [
       "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "glancingmind.cachix.org-1:FyFTWB9dixh0wjmhRGzqZDu5bCmhVlWdszIipVT/5vU="
     ];
   };
 }
