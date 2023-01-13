ca638a2c78c2960e44a9cb740da7e8e439542643
Sascha <GlancingMind@outlook.com>
2023-01-13 08:44:56 +0100

    Setup eduroam via activationscript instead of oneshot service

--- services/networking.nix
+++ services/networking.nix
@@ -61,7 +61,7 @@
     };
   };
 
-  systemd.services.iwd-config = let
+  system.activationScripts.setupEduroam = let
     eduroamCert = builtins.fetchurl {
       url = "https://www.pki.dfn.de/fileadmin/PKI/zertifikate/T-TeleSec_GlobalRoot_Class_2.pem";
       sha256 = "b30989fd9e45c74bf417df74d1da639d1f04d4fd0900be813a2d6a031a56c845";
@@ -70,25 +70,17 @@
       EAP-Method=PEAP
       EAP-Identity=eduroam@thm.de
       EAP-PEAP-Phase2-Method=MSCHAPV2
-      EAP-PEAP-ServerDomainMask=*.thm.de
+      EAP-PEAP-ServerDomainMask=wlan.thm.de
       EAP-PEAP-CACert=${eduroamCert}
       #ask on connection time or uncommend and enter user identity
       #EAP-PEAP-Phase2-Identity={{TH_IDENTITY}}
       ##ask on connection time or uncommend and enter password
       #EAP-PEAP-Phase2-Password={{TH_NETWORK_PW}}
-      #
-      #[Settings]
-      #AutoConnect=true
+
+      [Settings]
+      AutoConnect=true
     '';
-  in{
-    enable = true;
-    description = "IWD eduroam configurarion setup";
-    script = ''cp ${eduroamConfig} "/var/lib/iwd/eduroam.8021x"'';
-    wantedBy = [ "iwd.service" ];
-    before = [ "iwd.service" ];
-    #serviceConfig = {
-    #  Type = "oneshot";
-    #  RuntimeDirectoryPreserve = "yes";
-    #};
-  };
+  in ''
+    ln -sf ${eduroamConfig} /var/lib/iwd/eduroam
+  '';
 }
